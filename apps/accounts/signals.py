4badec3a391f8e73ca0745b217ad02b04ade1678
Erik Lundberg <erik.lundberg@folkhalsomyndigheten.se>
2017-08-01 15:39:42 +0200

    Add a receiver for the user_activated signal, to allow multiple registrations for the same id code.
    (cherry picked from commit f1b88c7)

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from registration.signals import user_registered
+from registration.signals import user_activated, user_registered
 
 from apps.accounts.models import user_profile
 from apps.survey.models import SurveyUser
@@ -11,16 +11,26 @@ def user_created(sender, user, request, **kwargs):
     """
     This signal is sent by the registration module when a new user is
     registered (before activation). This handler takes care of creating a
-    user_profile- and a SurveyUser instance for the new user. It will also
-    update the used SurveyIdCode with the information from the form.
+    user_profile instance for the new user.
     """
     form = CaptchaUnicodeRegistrationForm(request.POST)
 
-    profile = user_profile.objects.create(
+    user_profile.objects.create(
         user=user,
         yearofbirth=form.data['yearofbirth'],
         idcode=form.data['idcode']
     )
+
+
+@receiver(user_activated)
+def user_activated(sender, user, request, **kwargs):
+    """
+    This signal is sent by the registration module when a user is activated.
+    This handler takes care of creating a SurveyUser instance for the new user.
+    It will also update the used SurveyIdCode with the information from the
+    form.
+    """
+    profile = user_profile.objects.get(user=user)
 
     survey_user = SurveyUser.objects.create(
         user=user,
