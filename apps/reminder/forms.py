4de2ae5357cc3ee6643894eab56b891f9bae9b1a
Erik Lundberg <erik.lundberg@folkhalsomyndigheten.se>
2017-08-09 15:06:52 +0200

    Fix sorting of fields in news letter template admin view

--- apps/reminder/forms.py
+++ apps/reminder/forms.py
@@ -3,17 +3,25 @@ from datetime import datetime
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from djangocms_text_ckeditor.widgets import TextEditorWidget
-
 from hvad.forms import TranslatableModelForm
 
-from .models import NO_INTERVAL, ReminderSettings, NewsLetterTemplate, NewsLetter, get_default_for_newsitem, get_upcoming_dates, get_settings
+from .models import (
+    NO_INTERVAL, ReminderSettings, NewsLetterTemplate, NewsLetter,
+    get_default_for_newsitem, get_upcoming_dates, get_settings
+)
+
 
 class ReminderSettingsForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super(ReminderSettingsForm, self).clean()
         if cleaned_data.get('send_reminders'):
-            if cleaned_data.get('interval') != NO_INTERVAL and not (cleaned_data.get('begin_date') and cleaned_data.get('interval')):
-                raise forms.ValidationError(_("If 'send_reminders' is checked the other fields pertaining to reminders are required"))
+            if (cleaned_data.get('interval') != NO_INTERVAL
+                    and not (cleaned_data.get('begin_date')
+                             and cleaned_data.get('interval'))):
+                raise forms.ValidationError(
+                    _("If 'send_reminders' is checked the other fields "
+                      "pertaining to reminders are required")
+                )
         return cleaned_data
 
     class Meta:
@@ -26,11 +34,21 @@ class NewsLetterTemplateForm(TranslatableModelForm):
 
     class Meta:
         model = NewsLetterTemplate
+        fields = (
+            'is_default_reminder',
+            'is_default_newsitem',
+            'sender_email',
+            'sender_name',
+            'subject',
+            'message'
+        )
 
 
 class NewsLetterForm(TranslatableModelForm):
     message = forms.CharField(widget=TextEditorWidget())
-    date = forms.ChoiceField(help_text=_("If no dates are shown here, check your site settings"))
+    date = forms.ChoiceField(
+        help_text=_("If no dates are shown here, check your site settings")
+    )
 
     def __init__(self, *args, **kwargs):
         super(NewsLetterForm, self).__init__(*args, **kwargs)
@@ -39,15 +57,17 @@ class NewsLetterForm(TranslatableModelForm):
             self.fields['date'] = forms.DateField()
         else:
             self.fields['date'].choices = get_upcoming_dates(datetime.now())
-            
 
-        # I don't understand where self.language comes from, it must be some magic in TranslatableModelForm
-        # but I can't identify it; in any case we need some default for tests
+        # I don't understand where self.language comes from, it must be some
+        # magic in TranslatableModelForm but I can't identify it; in any case
+        # we need some default for tests
         if not hasattr(self, 'language'):
             self.language = 'en'
 
-        for fieldname in ['sender_email', 'sender_name', 'subject', 'message']:
-            self.fields[fieldname].initial = getattr(get_default_for_newsitem(self.language), fieldname) if get_default_for_newsitem(self.language) else "" 
+        for fieldname in ('sender_email', 'sender_name', 'subject', 'message'):
+            self.fields[fieldname].initial = getattr(
+                get_default_for_newsitem(self.language), fieldname
+            ) if get_default_for_newsitem(self.language) else ""
 
     class Meta:
         model = NewsLetter
